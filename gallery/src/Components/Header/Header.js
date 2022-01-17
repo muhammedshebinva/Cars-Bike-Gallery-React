c4c59555508ae23b66f8e8368c7b87b9afd826a9
muhammedshebinva <muhammedshebinva@gmail.com>
2022-01-17 14:17:14 +0530

    nav edited

--- gallery/src/Components/Header/Header.js
+++ gallery/src/Components/Header/Header.js
@@ -7,7 +7,7 @@ function Header() {
         <div>
             <Navbar bg="dark" variant="dark">
     <Container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Navbar.Brand href="#home">GT</Navbar.Brand>
     <Nav className="me-auto">
       <Nav.Link href="/">Home</Nav.Link>
       <Nav.Link href="/cars">Cars</Nav.Link>
