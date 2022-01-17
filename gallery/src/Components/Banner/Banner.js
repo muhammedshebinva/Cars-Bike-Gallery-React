3f8d652d4f076352a0f27a2bda4ed16d7578c74e
muhammedshebinva <muhammedshebinva@gmail.com>
2022-01-17 14:18:01 +0530

    banner added backgrownd img and btn,txt

---
+++ gallery/src/Components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Banner/Banner.css'
+function Banner() {
+    return (
+        <div className='banner'>
+            <div className="bannerContent">
+                <h1>My Favorite Cars & Bikes</h1>
+                <button className='button'>Add to Colletion</button>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
